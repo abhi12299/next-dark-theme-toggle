884408d75bfc71dd5959704fa99c95a40839099b
Abhishek M <abhishek.m@so.city>
2020-05-19 18:16:50 +0530

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <ul>
+      <li>
+        <Link href="/b" as="/a">
+          <a>a</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/a" as="/b">
+          <a>b</a>
+        </Link>
+      </li>
+    </ul>
+  )
+}
