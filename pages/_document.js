59b934102646b9ea9f69c8554cf3d02f829152c3
Abhishek M <abhishek.m@so.city>
2020-05-19 21:38:50 +0530

    remove debug statements

--- pages/_document.js
+++ pages/_document.js
@@ -3,16 +3,13 @@ import Cookies from 'js-cookie'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    console.log('GIP called')
     let theme
     if (ctx.req && 'cookies' in ctx.req) {
       const { req } = ctx
-      console.log('server cookies are:', req.cookies)
 
       theme = req.cookies.theme || 'light'
     } else {
       theme = Cookies.get('theme') === 'dark' ? 'dark' : 'light'
-      console.log('client cookies are:', Cookies.get('theme'))
     }
 
     const initialProps = await Document.getInitialProps(ctx)
